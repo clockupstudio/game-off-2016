c303bb3b72e32a1b0e6326f0a25edf1b65166f01
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-11-06 01:01:01 +0700

    Introduce "small fish"
    Ref #4

--- src/war_state.ts
+++ src/war_state.ts
@@ -1,16 +1,20 @@
 import * as Phaser from "phaser";
 import { Her } from "./her";
 
+const SPRITESHEETS_PATH = "assets/spritesheets";
+
 export class WarState extends Phaser.State {
 
     private her: Her;
 
     preload() {
-        this.game.load.image("her", "assets/spritesheets/main_char.png");
+        this.game.load.image("her", `${SPRITESHEETS_PATH}/main_char.png`);
+        this.game.load.image("small_fish", `${SPRITESHEETS_PATH}/small_fish.png`);
     }
 
     create() {
         this.her = Her.create(this.game);
+        SmallFish.create(this.game);
     }
 
     update() {
@@ -25,4 +29,11 @@ export class WarState extends Phaser.State {
         }
     }
 
+}
+
+class SmallFish {
+    static create(game: Phaser.Game) {
+        let sprite = game.add.sprite(game.world.centerX, game.world.centerY - 300, "small_fish");
+        sprite.anchor.setTo(0.5);
+    }
 }
