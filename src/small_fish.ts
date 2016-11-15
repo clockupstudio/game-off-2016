9a67d67a1e02feea603b3e0b5ba07db8acd1a77d
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-11-15 23:32:05 +0700

    Small Fish can move to player and disappear when go outside of game.
    Close #6

--- src/small_fish.ts
+++ src/small_fish.ts
@@ -1,8 +1,27 @@
 import * as Phaser from "phaser";
 
+const VELOCITY = 10;
+
 export class SmallFish {
-    static create(game: Phaser.Game) {
+
+    constructor(private sprite: Phaser.Sprite) {
+    }
+
+    move() {
+        this.sprite.y += VELOCITY;
+    }
+
+    movedOutOfGame(): boolean {
+        return (this.sprite.y > 1080);
+    }
+
+    destroy() {
+        this.sprite.destroy();
+    }
+
+    static create(game: Phaser.Game): SmallFish {
         let sprite = game.add.sprite(game.world.centerX, game.world.centerY - 300, "small_fish");
         sprite.anchor.setTo(0.5);
+        return new SmallFish(sprite);
     }
 }
