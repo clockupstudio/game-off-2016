d54df7f28bf71a9c40874091d96273b9d7400cf5
Prayoch Rujira <jay@odd-e.co.th>
2016-11-27 19:16:37 +0700

    add IE's animations

--- src/ie.ts
+++ src/ie.ts
@@ -4,6 +4,11 @@ export default class IE extends Phaser.Sprite {
 
     constructor(game: Phaser.Game, x: number, y: number) {
         super(game, x, y, "boss_ie");
+
+        this.animations.add("idle", [0], 1, true);
+        this.animations.add("die", [1], 1, true);
+
+        this.animations.play("idle")
     }
 
     update() {
