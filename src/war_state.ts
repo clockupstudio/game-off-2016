f6d696c85a70cd5c6be6800aad36669349ba3a25
Prayoch Rujira <ninjaadvance@gmail.com>
2016-11-27 14:36:52 +0700

    Power items (#62)
    * Add Powerup item
    - Update level map
    - Add class Powerup
    - Add createItems() in WarState
    - Remove duplicate code
    
    * Change from Array.forEach to (Lodash).foreach
    
    * Add movement to powerup item
    	- Add update() to PowerUp
    	- Call PowerUp.update() in WarState.update()
    
    * Replace using PowerUpGroup as native Array to class PowerUpGroup
    
    * Add more powerup items
    
    * Format code
    
    Close #16

--- src/war_state.ts
+++ src/war_state.ts
@@ -3,6 +3,9 @@ import { Her } from "./her";
 import { SmallFish } from "./small_fish";
 import * as _ from "lodash";
 import StageBackground from "./stage_background";
+import { PowerUp } from "./power_up";
+import PowerUpGroup from "./power_up_group";
+
 
 const SPRITESHEETS_PATH = "assets/spritesheets";
 const SOUNDS_PATH = "assets/sounds";
@@ -16,12 +19,15 @@ export class WarState extends Phaser.State {
     private backgroundLayer: Phaser.TilemapLayer;
     private enemyGroup: SmallFish[];
     private stageBackground: StageBackground;
+    private powerUpGroup: PowerUpGroup; 
+
 
     preload() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.load.image("her", `${SPRITESHEETS_PATH}/main_char.png`);
         this.game.load.image("small_fish", `${SPRITESHEETS_PATH}/small_fish.png`);
         this.game.load.image("bullet", `${SPRITESHEETS_PATH}/bullet.png`);
+        this.game.load.image("power_up", `${SPRITESHEETS_PATH}/item.png`);
         this.game.load.audio("shooting", `${SOUNDS_PATH}/shooting.ogg`);
         this.game.load.tilemap("level_01", `${TILEMAPS_PATH}/level_01.json`, null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image("stage_background", `${SPRITESHEETS_PATH}/stage.png`);
@@ -37,29 +43,16 @@ export class WarState extends Phaser.State {
 
         this.createHer();
         this.createEnemies();
+        this.createItems();
         this.game.camera.y = 2280;
     }
 
-    createEnemies() {
-        
-        this.enemyGroup = [];
-
-        let origins = _.filter(this.levelMap.objects["Player"], (mapObject: any) => {
-            return mapObject.type === "enemy";
-        });
-
-        origins.forEach((element) => {
-            this.enemyGroup.push(SmallFish.create(this.game, element.x, element.y - 40));
-
-        });
-    }
-
     createHer() {
-        let position: { x:number, y: number} = this.findHerOrigin();
+        let position: { x: number, y: number } = this.findHerOrigin();
         this.her = Her.create(this.game, position.x, position.y);
     }
 
-    findHerOrigin(): {x: number, y: number} {
+    findHerOrigin(): { x: number, y: number } {
 
         let playerOrigin = _.find(this.levelMap.objects["Player"], (mapObject: any) => {
             return mapObject.type === "player_start";
@@ -72,6 +65,35 @@ export class WarState extends Phaser.State {
 
     }
 
+    createEnemies() {
+        this.enemyGroup = [];
+
+        this.findObjectOrigins("enemy").forEach((element) => {
+            this.enemyGroup.push(SmallFish.create(this.game, element.x, element.y - 40));
+
+        });
+    }
+
+    createItems() {
+        this.powerUpGroup = new PowerUpGroup();
+
+        this.findObjectOrigins("item").forEach((element) => {
+            this.powerUpGroup.add(PowerUp.create(this.game, element.x, element.y));
+        });
+    }
+
+    createObjectFromMap(type: string, group: any[], createFunction: () => {}) {
+        this.findObjectOrigins(type).forEach((element) => {
+            group.push(createFunction());
+        });
+    }
+
+    findObjectOrigins(type: string): { x: number, y: number }[] {
+        return _.filter(this.levelMap.objects["Player"], (mapObject: any) => {
+            return mapObject.type === type;
+        });
+    }
+
     update() {
         this.stageBackground.update();
 
@@ -80,10 +102,13 @@ export class WarState extends Phaser.State {
             smallFish.update()
         });
         this.enemyGroup.forEach((smallFish: SmallFish) => {
-            this.game.physics.arcade.collide(smallFish.sprite, this.her.sprite, ()=>{
+            this.game.physics.arcade.collide(smallFish.sprite, this.her.sprite, () => {
                 this.her.destroy();
             });
         });
+
+        this.powerUpGroup.checkCollisionWith(this.game, this.her);
+        this.powerUpGroup.update();
     }
 
     render() {
