bd35f202f29c7e951836d5912a44590cd640430f
Prayoch Rujira <jay@odd-e.co.th>
2016-11-27 19:51:21 +0700

    Scripted IE to die when reach the assigned point

--- src/ie.ts
+++ src/ie.ts
@@ -1,17 +1,33 @@
 export default class IE extends Phaser.Sprite {
 
     private VELOCITY: number = 1;
+    private dead: boolean = false;
 
     constructor(game: Phaser.Game, x: number, y: number) {
         super(game, x, y, "boss_ie");
 
-        this.animations.add("idle", [0], 1, true);
-        this.animations.add("die", [1], 1, true);
+        this.animations.add(IEAnimations.IDLE, [0], 1, true);
+        this.animations.add(IEAnimations.DIE, [1], 1, true);
 
-        this.animations.play("idle")
+        this.animations.play(IEAnimations.IDLE);
     }
 
     update() {
+        if(this.dead){
+            return;
+        }
+
+        if(this.y > (this.game.world.height-1000)){
+            this.animations.play(IEAnimations.DIE);
+            this.dead = true;
+            return;
+        }
+
         this.y += this.VELOCITY;
     }
+}
+
+const IEAnimations = {
+    IDLE: "idle",
+    DIE: "die"
 }
