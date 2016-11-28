8eee0b330074921832b1724237d627ee8ac5d402
Prayoch Rujira <ninjaadvance@gmail.com>
2016-11-28 21:47:57 +0700

    Medium fish can shoot bullet (#81)
    * Medium fish can shoot the bullet
    
    * Medium fish can shoot bullet
    
    * Extract shooting evil bullet to EvilBulletShooter and EvilBullet class
    
    * Scale 2x to evil bullet
    
    * Add more medium fished
    
    Close #9

---
+++ src/evil_bullet.ts
@@ -0,0 +1,20 @@
+import { Her } from "./her";
+
+export class EvilBullet extends Phaser.Sprite {
+    constructor(game: Phaser.Game, x: number, y: number) {
+        super(game, x, y, "evil_bullet")
+        this.scale.setTo(2,2);
+    }
+}
+
+export class EvilBulletShooter {
+
+    constructor(private game: Phaser.Game) { }
+
+    shootTo(originX: number, originY: number, target: Phaser.Sprite) {
+        let evilBullet: EvilBullet = new EvilBullet(this.game, originX, originY);
+        this.game.world.add(evilBullet);
+        this.game.physics.arcade.enable(evilBullet);
+        this.game.physics.arcade.moveToObject(evilBullet, target, 300);
+    }
+}
--- src/medium_fish.ts
+++ src/medium_fish.ts
@@ -1,17 +1,31 @@
+import { Her } from "./her";
+import { EvilBulletShooter } from "./evil_bullet";
+
 export default class MediumFish extends Phaser.Sprite {
 
     private VELOCITY: number = 5;
+    private shoot: boolean = false;
+    private evilBulletShooter: EvilBulletShooter = new EvilBulletShooter(this.game);
 
-    constructor(game: Phaser.Game, x: number, y: number) {
+    constructor(game: Phaser.Game, x: number, y: number, private her:Her) {
         super(game, x, y, "medium_fish");
         game.physics.arcade.enable(this);
     }
 
     update() {
         this.y += this.VELOCITY;
+
+        if(this.shoot){
+            return;
+        }
+
+        if((this.her.sprite.y - this.y ) <= 640 ) {
+            this.shoot = true;
+            this.evilBulletShooter.shootTo(this.x+40, this.y+80, this.her.sprite);
+        }
     }
 
-    static create(game: Phaser.Game, x: number, y: number): MediumFish {
-        return new MediumFish(game, x, y);
+    static create(game: Phaser.Game, x: number, y: number, her: Her): MediumFish {
+        return new MediumFish(game, x, y, her);
     }
 }
