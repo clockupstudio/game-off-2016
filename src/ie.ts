e76f40e9af860d9cb93d489ddf8d3078a1bbf221
Prayoch Rujira <jay@odd-e.co.th>
2016-11-29 22:57:56 +0700

    Play bomb sound when dead

--- src/ie.ts
+++ src/ie.ts
@@ -5,6 +5,7 @@ export default class IE extends Phaser.Sprite {
 
     private VELOCITY: number = 1;
     private dead: boolean = false;
+    private bombSound: Phaser.Sound;
 
     constructor(game: Phaser.Game, x: number, y: number) {
         super(game, x, y, "boss_ie");
@@ -13,6 +14,7 @@ export default class IE extends Phaser.Sprite {
         this.animations.add(IEAnimations.DIE, [1], 1, true);
 
         this.animations.play(IEAnimations.IDLE);
+        this.bombSound = new Phaser.Sound(game, "bomb");
     }
 
     update() {
@@ -31,6 +33,7 @@ export default class IE extends Phaser.Sprite {
     }
 
     selfExplode() {
+        this.bombSound.play();
         _.each(this.generateGridPoints(4, 3), (point:{x: number, y: number})=> {
             let newExplosion:explosion.Explosion = explosion.mediumExplosion(this.game, this.x+80+(80*point.x), this.y+80+(80*point.y));
             newExplosion.explode().onComplete.add(() => {
