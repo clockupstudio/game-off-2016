e922050d3ffd7a34d0ff0f3d01ce42bd92853596
Prayoch Rujira <ninjaadvance@gmail.com>
2016-11-29 22:26:58 +0700

    Evil bullet can kill Her (#85)
    - Add EvilBulletGroup to WarState
    - Assign to EvilBulletShooter via MediumFish
    - Add collicsion detection between evil bullet and Her
    
    Close #22

--- src/medium_fish.ts
+++ src/medium_fish.ts
@@ -5,11 +5,12 @@ export default class MediumFish extends Phaser.Sprite {
 
     private VELOCITY: number = 5;
     private shoot: boolean = false;
-    private evilBulletShooter: EvilBulletShooter = new EvilBulletShooter(this.game);
+    private evilBulletShooter: EvilBulletShooter;
 
-    constructor(game: Phaser.Game, x: number, y: number, private her:Her) {
+    constructor(game: Phaser.Game, x: number, y: number, private her:Her, private evilBulletGroup: Phaser.Group) {
         super(game, x, y, "medium_fish");
         game.physics.arcade.enable(this);
+        this.evilBulletShooter = new EvilBulletShooter(this.game, evilBulletGroup);
     }
 
     update() {
@@ -25,7 +26,7 @@ export default class MediumFish extends Phaser.Sprite {
         }
     }
 
-    static create(game: Phaser.Game, x: number, y: number, her: Her): MediumFish {
-        return new MediumFish(game, x, y, her);
+    static create(game: Phaser.Game, x: number, y: number, her: Her, evilBulletGroup: Phaser.Group): MediumFish {
+        return new MediumFish(game, x, y, her, evilBulletGroup);
     }
 }
