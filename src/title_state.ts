b7ab785ba5d489778549158c7b85cb86071d05bf
Prayoch Rujira <jay@odd-e.co.th>
2016-11-30 20:52:28 +0700

    Add keypress event to start the game Remove dupicated code to function

--- src/title_state.ts
+++ src/title_state.ts
@@ -1,21 +1,19 @@
 export class TitleState extends Phaser.State {
 
     create() {
-        let aboveTitleStyle = { font: "bold 18px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        this.game.add.text(this.game.world.centerX, this.game.world.centerY-280, "Github game-off 2016", aboveTitleStyle).anchor.setTo(0.5, 0.5);
-
-        let titleStyle = { font: "bold 128px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        this.game.add.text(this.game.world.centerX, this.game.world.centerY-200, "F5", titleStyle).anchor.setTo(0.5, 0.5);
-
-        let messageStyle = { font: "bold 48px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        this.game.add.text(this.game.world.centerX, this.game.world.centerY+200, "PRESS ANY KEY TO START", messageStyle).anchor.setTo(0.5, 0.5);
-
-        let teamStyle = { font: "bold 24px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        this.game.add.text(this.game.world.centerX, this.game.world.height - 32, "2016 Clockup studio", teamStyle).anchor.setTo(0.5, 0.5);
-
-        this.game.input.keyboard.addCallbacks(this, null, null, ()=> {
+        this.createText("Github game-off 2016", 18, -280);
+        this.createText("F5", 128, -200);
+        this.createText("PRESS ANY KEY TO START", 48, 200);
+        this.createText("2016 Clockup studio", 24, 500);
+        
+        this.game.input.keyboard.onPressCallback = ()=> {
+            this.input.keyboard.onPressCallback = null;
             this.game.state.start("war");
-        });
+        };
     }
 
+    createText(text: string, size: number, offsetY: number) {
+        let aboveTitleStyle = { font: `bold ${size}px Arial`, fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        this.game.add.text(this.game.world.centerX, this.game.world.centerY+offsetY, text, aboveTitleStyle).anchor.setTo(0.5, 0.5);
+    }
 }
